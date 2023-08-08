41f8d9a3698d08153a93645d3f2cd3c56af74cad
Iamneo <techadmin@iamneo.ai>
2023-08-08 16:31:17 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CurrconvPipe } from './currconv.pipe';
+import { RemovCommaPipe } from './remov-comma.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CurrconvPipe,
+    RemovCommaPipe,
   
   ],
   imports: [
---
+++ angularapp/src/app/remov-comma.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RemovCommaPipe } from './remov-comma.pipe';
+
+describe('RemovCommaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RemovCommaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
